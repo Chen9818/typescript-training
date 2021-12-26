37ce690b189e8f9f19837ba8af5031a6b141af94
Chen9818 <88109684+Chen9818@users.noreply.github.com>
2021-12-26 13:46:29 +0800

    update ts-training

---
+++ src/1-type.ts
@@ -0,0 +1,20 @@
+//基本類型
+let aa: string = "aa"
+let str: string
+str = "hello"
+
+let num: number = 1000
+let bool: boolean = true
+let n: null = null
+let un: undefined = undefined
+let an: any = true
+
+//陣列
+let arr: string[] = ["a", "s"]
+let arr2: string[][] = [["a", "s"]]
+
+//元組
+let tuple: [number, string, boolean] = [1, "a", false]
+let tuple2: [number, string][] = [[1, "a"]]
+
+//enum枚舉
